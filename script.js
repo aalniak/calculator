a307e160a6439e378cfa9ea9c6b9501f1d648d6c
Arda <arda.alniak@metu.edu.tr>
2022-09-16 18:17:17 +0300

    build a version that can continuously calculate

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function operate(operator,numberArray){
         return 0
     }
 
-    result = operator(parseInt(numberArray[0]),parseInt(numberArray[1]))
+    result = operator(parseFloat(numberArray[0]),parseFloat(numberArray[1]))
     calculatorScreen.value = result
     return result
 }
@@ -39,9 +39,7 @@ Array.from(numberButtons).map(button => button.addEventListener('click',(e)=>{
 }))
 
 operatorButtons = document.querySelectorAll(".operator");
-Array.from(operatorButtons).map(button => button.addEventListener('click',(e)=>{
-    calculatorScreen.value+=e.target.textContent
-}))
+
 
 function numberDetect(inputValue){
     if (!inputValue){
@@ -101,10 +99,42 @@ function operatorDetect(inputValue){
         console.log(calculatorScreen.value)
     }
 }
+let tempOperator = null;
+let opertorBefore = null;
+let temporaryNumber = null;
+let temporaryNumber2 = null;
+function opButtonBehavior(stringElement){
+    
+    console.log(calculatorScreen.value)
+    
+
+    console.log(temporaryNumber2);
+    console.log(calculatorScreen.value)
+    
+    let tempResult= null;
+
+    if (!operatorExistsCheck(calculatorScreen.value)){
+        console.log(calculatorScreen.value)
+        temporaryNumber = calculatorScreen.value;
+        console.log(temporaryNumber);
+        
+
+    }
+    else {
+        temporaryNumber2 = calculatorScreen.value.replace(temporaryNumber,"").replace(operatorBefore,"")
+        console.log(temporaryNumber + "tn")
+        console.log(temporaryNumber2 + "tn2");
+        tempResult = operate(operatorDetect(operatorBefore),[parseFloat(temporaryNumber),parseFloat(temporaryNumber2)] );
+        console.log(tempResult);
+        (e)=>{
+            calculatorScreen.value+=e.target.textContent
+            tempOperator = e.target.textContent;
+        }
+        temporaryNumber = tempResult;
+    }  }
 
 equalsButton = document.querySelector('.equals');
-equalsButton.addEventListener('click',()=>operate(operatorDetect(calculatorScreen.value),numberDetect(calculatorScreen.value)))
-
+equalsButton.addEventListener('click',()=> operate(operatorDetect(calculatorScreen.value),numberDetect(calculatorScreen.value)))
 
 function clearScreen(){
     calculatorScreen.value="";
@@ -122,14 +152,22 @@ function backspaceHit(){
 backspaceButton = document.querySelector('.backspace');
 backspaceButton.addEventListener('click', ()=> backspaceHit())
 
+Array.from(operatorButtons).map(button => button.addEventListener('click',(e)=>{
+    operatorBefore = tempOperator;
+    tempOperator = e.target.textContent;
+}))
 
+Array.from(operatorButtons).map(item=>item.addEventListener('click',()=>opButtonBehavior(calculatorScreen.value)))
 
-
+Array.from(operatorButtons).map(button => button.addEventListener('click',(e)=>{
+    calculatorScreen.value+=e.target.textContent
+    
+}))
 
 //// 12+6-3
 
-function operatorExistsCheck(stringElement){ //gives true if operator already exists
-    arrayElement = stringElement.split("");4
+function operatorExistsCheck(stringElement){ //gives true if any operator already exists
+    arrayElement = stringElement.split("");
     if (arrayElement.filter(element=>{return element === "+"}).length || 
     arrayElement.filter(element=>{return element === "/"}).length || 
     arrayElement.filter(element=>{return element === "*"}).length || 
@@ -140,27 +178,4 @@ function operatorExistsCheck(stringElement){ //gives true if operator already ex
         return false
     }
 
-function buttonMemory(stringElement){
-    temporaryNumber = calculatorScreen.value;
-    temporaryNumber2 = null;
-    (e)=>{calculatorScreen.value+=e.target.textContent}
-    let tempOperator= null;
-    let tempResult= null;
-
-    if (!operatorExistsCheck(calculatorScreen.value)){
-        temporaryNumber = parseInt(calculatorScreen.value);
-        
-        (e)=>{
-            calculatorScreen.value+=e.target.textContent
-            tempOperator = e.target.textContent;
-        }
-
-    }
-    else {
-        temporaryNumber2 = calculatorScreen.value.replace(temporaryNumber,"").replace(tempOperator,"")
-        tempResult = operate(operatorDetect(tempOperator.split("")),[parseFloat(temporaryNumber),parseFloat(temporaryNumber2)] );
-        (e)=>{
-            calculatorScreen.value+=e.target.textContent
-            tempOperator = e.target.textContent;
-        }
-    }  }}
+}
